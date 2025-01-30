d818ccea2944eefe41e61be1dd6af48242ea3fb7
flowu <ffl0w.uuuu@gmail.com>
2025-01-30 18:10:00 +0800

    added get pid

--- sigma-mapper/include/ftil.hpp
+++ sigma-mapper/include/ftil.hpp
@@ -5,7 +5,16 @@
 #include <TlHelp32.h>
 
 struct FTIL {
-    DWORD getPId() {}
+    DWORD getPId(const char* procName) {
+        PROCESSENTRY32 procEntry = {sizeof(PROCESSENTRY32)};
+        SMART_HANDLE hSnap       = CreateToolhelp32Snapshot(TH32CS_SNAPALL, 0);
 
-    bool getFileBuffer() {}
+        if (Process32First(hSnap, &procEntry)) {
+            do {
+                if (!strcmp(procEntry.szExeFile, procName)) return procEntry.th32ProcessID;
+
+            } while (Process32Next(hSnap, &procEntry));
+        }
+    }
 };
+inline FTIL* util;
