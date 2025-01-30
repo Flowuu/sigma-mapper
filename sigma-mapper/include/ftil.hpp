5790b597b55cb9ed3d6ed97d87fc46be5e07b131
flowu <ffl0w.uuuu@gmail.com>
2025-01-30 15:03:16 +0800

    add util

--- sigma-mapper/include/ftil.hpp
+++ sigma-mapper/include/ftil.hpp
@@ -6,17 +6,19 @@
 
 struct SMART_HANDLE {
    private:
-    HANDLE m_handle{INVALID_HANDLE_VALUE};
+    HANDLE m_handle = INVALID_HANDLE_VALUE;
 
    public:
     SMART_HANDLE() = default;
 
     SMART_HANDLE(HANDLE inHandle) : m_handle(inHandle) {}
 
+    SMART_HANDLE(SMART_HANDLE&& other) noexcept : m_handle(other.m_handle) { other.m_handle = INVALID_HANDLE_VALUE; }
+
+    ~SMART_HANDLE() { close(); }
+
     SMART_HANDLE(const SMART_HANDLE&)            = delete;
     SMART_HANDLE& operator=(const SMART_HANDLE&) = delete;
-
-    SMART_HANDLE(SMART_HANDLE&& other) noexcept : m_handle(other.m_handle) { other.m_handle = INVALID_HANDLE_VALUE; }
 
     SMART_HANDLE& operator=(SMART_HANDLE&& other) noexcept {
         if (this != &other) {
@@ -27,13 +29,13 @@ struct SMART_HANDLE {
         return *this;
     }
 
-    ~SMART_HANDLE() { close(); }
-
     void close() {
         if (m_handle != INVALID_HANDLE_VALUE) {
             CloseHandle(m_handle);
             m_handle = INVALID_HANDLE_VALUE;
         }
+
+        printf("deleted");
     }
 
     HANDLE get() const noexcept { return m_handle; }
